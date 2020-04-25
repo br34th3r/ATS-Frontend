ead405a7e792b05eba6125d3306d1f944c679bd3
Kamila <kamilamocek@gmail.com>
2020-04-25 11:23:38 +0200

    Small fix

--- src/services/auth.js
+++ src/services/auth.js
@@ -40,7 +40,7 @@ export const handleLogin = ({ username, password }) => {
 }
 
 export const logout = function() {
-  fetch('http://localhost:80/logout', {
+  fetch(`${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}/logout`, {
     mathod: "GET",
     mode: "cors",
     redirect: 'follow'
